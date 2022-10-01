875d7b7ce84aa8610e05e45ebc0dae24351bc670
Mate Andras Magyar <mate.magyar@ukatemi.com>
2022-10-01 15:17:46 +0200

    Implement CAFF animation loading

---
+++ libCAFF/CAFFAnimation.h
@@ -0,0 +1,66 @@
+//
+// Created by Barrow099 on 2022. 10. 01..
+//
+
+#ifndef LIBCAFF_CAFFANIMATION_H
+#define LIBCAFF_CAFFANIMATION_H
+
+#include "CIFFImage.h"
+#include <cstdint>
+#include <string>
+struct CAFFCreditsFrame {
+    uint16_t YY;
+    uint8_t M;
+    uint8_t D;
+    uint8_t h;
+    uint8_t m;
+    std::string creator;
+
+public:
+    CAFFCreditsFrame(uint16_t yy, uint8_t m, uint8_t d, uint8_t h, uint8_t m1, const std::string &creator);
+    CAFFCreditsFrame();
+};
+
+class CAFFAnimationFrame {
+    uint64_t duration;
+    CIFFImage *image;
+
+public:
+    CAFFAnimationFrame(uint64_t duration, CIFFImage *image);
+    CIFFImage *getImage() const;
+    int64_t getDuration() const;
+};
+
+struct CAFFHeaderBlock {
+    char magic[4];
+    int64_t header_size;
+    int64_t num_anim;
+    CAFFHeaderBlock(uint8_t* buff, size_t len);
+};
+
+class CAFFBlock {
+    uint8_t type;
+    uint64_t length;
+    uint8_t *data;
+
+
+public:
+    CAFFBlock(uint8_t type, uint64_t length, uint8_t *data);
+    explicit CAFFBlock();
+    uint8_t getType() const;
+    void setType(uint8_t type);
+    uint64_t getLength() const;
+    void setLength(uint64_t length);
+    uint8_t *getData() const;
+    void setData(uint8_t *data);
+};
+
+struct CAFFAnimation {
+    CAFFCreditsFrame credits;
+    std::vector<CAFFAnimationFrame> frames;
+public:
+    CAFFAnimation(CAFFCreditsFrame credits, std::vector<CAFFAnimationFrame> frames);
+};
+
+
+#endif//LIBCAFF_CAFFANIMATION_H
--- libCAFF/exe_test.cpp
+++ libCAFF/exe_test.cpp
@@ -1,6 +1,7 @@
 //
 // Created by Barrow099 on 2022. 09. 30..
 //
+#include "CAFFLoader.h"
 #include "CIFFLoader.h"
 #include "CIFFWriter.h"
 #include <fstream>
@@ -8,15 +9,15 @@
 #include <stdlib.h>
 
 
-int main(int argc, char ** argv) {
+int main(int argc, char **argv) {
     FILE *f = fopen("test_images/1.ciff", "rb");
-    if(!f) {
+    if (!f) {
         printf("failed to open file\n");
         return 1;
     }
     fseek(f, 0, SEEK_END);
     long fsize = ftell(f);
-    fseek(f, 0, SEEK_SET);  /* same as rewind(f); */
+    fseek(f, 0, SEEK_SET); /* same as rewind(f); */
 
     char *string = new char[fsize];
     fread(string, fsize, 1, f);
@@ -24,7 +25,7 @@ int main(int argc, char ** argv) {
 
 
     CIFFImage *image = CIFFLoader::from_bytes((uint8_t *) string, fsize);
-    if(image == nullptr) {
+    if (image == nullptr) {
         printf("error: %s\n", CIFFLoader::error_message());
         return 1;
     }
@@ -35,5 +36,21 @@ int main(int argc, char ** argv) {
     printf("%lldx%lld\n", image->getWidth(), image->getHeight());
 
     CIFFWriter::write_to_file(image, "out.ciff");
+
+
+    CAFFAnimation *anim = CAFFLoader::from_file("test_images/3_fixed.caff");
+    if(anim == nullptr) {
+        printf("CAFFLoader failed: %s\n", CAFFLoader::get_error_message());
+    }
+    printf("Loaded %lu frames by %s\n", anim->frames.size(), anim->credits.creator.c_str());
+    int id = 0;
+    for (const auto &item: anim->frames) {
+        char filename[128];
+        snprintf(filename, 127, "frame_%d.rgb", id++);
+        printf("frame %d: %lldx%lld, duration: %lld\n", id-1, item.getImage()->getWidth(), item.getImage()->getHeight(), item.getDuration());
+        FILE *out = fopen(filename, "wb");
+        fwrite(item.getImage()->getContent(), 1, item.getImage()->getContent_size(), out);
+        fclose(out);
+    }
     return 0;
 }
