75eb0c4836872b153ea660a44413160f03574645
Surmann Roland <surmannroland@gmail.com>
2022-10-24 13:33:14 +0200

    Create DTOs for CaffFile requests

---
+++ 3de0/3de0_BLL/Dtos/UploadCaffFileDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3de0_BLL.Dtos
+{
+    public class UploadCaffFileDto
+    {
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public byte[] File { get; set; }
+    }
+}
