723b7672aafff458b0f622191cb84101cc65bfc0
Surmann Roland <surmannroland@gmail.com>
2022-10-24 15:51:39 +0200

    Added missing column in files

--- 3de0/3de0_BLL_DAL/CaffFile.cs
+++ 3de0/3de0_BLL_DAL/CaffFile.cs
@@ -8,8 +8,9 @@ namespace _3de0_BLL_DAL
     public class CaffFile
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Caption { get; set; }
         public DateTime CreationDate { get; set; }
+        public string Creator { get; set; }
         public int Price { get; set; }
         public string FilePath { get; set; }
 
