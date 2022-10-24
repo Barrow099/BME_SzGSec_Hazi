a9f6e48bfc5aba88ecc7a6d2849e23c5feda46e1
Surmann Roland <surmannroland@gmail.com>
2022-10-24 14:46:23 +0200

    CommentService created for comment operations

---
+++ 3de0/3de0_BLL/ICommentService.cs
@@ -0,0 +1,17 @@
+﻿using _3de0_BLL.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3de0_BLL
+{
+    public interface ICommentService
+    {
+        Task ModifyCommentById(int id, CreateCommentDto modifyComment, string userId);
+        Task RemoveCommentById(int id, string userId);
+
+        Task<CommentDto> AddComment(CreateCommentDto comment);
+    }
+}
