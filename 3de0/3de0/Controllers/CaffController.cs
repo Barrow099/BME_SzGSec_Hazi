3ed1b9dac3573bd9b53c2c6db279669be9c25e7c
Surmann Roland <surmannroland@gmail.com>
2022-11-13 18:48:23 +0100

    Backend functions

--- 3de0/3de0/Controllers/CaffController.cs
+++ 3de0/3de0/Controllers/CaffController.cs
@@ -2,6 +2,8 @@ using _3de0_BLL;
 using _3de0_BLL.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http.Headers;
 using System.Security.Claims;
 
 namespace _3de0.Controllers
@@ -12,6 +14,8 @@ namespace _3de0.Controllers
     public class CaffController : ControllerBase
     {
         private readonly ICaffService _caffService;
+        private readonly string pathDir = "CaffFiles/";
+
         public CaffController(ICaffService caffService)
         {
             _caffService = caffService;
@@ -43,17 +47,42 @@ namespace _3de0.Controllers
             return File(file, "application/octet-stream");
         }
 
+        [Route("{id}/preview")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetImagePreviewByCaffId(int id)
+        {
+            var file = await _caffService.GetPreviewImageByCaffId(id);
+            return File(file, "image/png");
+        }
+
         [Route("{id}")]
         [HttpPut]
         [Authorize(Roles = "admin")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> ModifyCaffFile(int id, [FromBody] UploadCaffFileDto modifyCaffFile)
+        public async Task<IActionResult> ModifyCaffFile(int id, [FromForm] UploadCaffFileDto modifyCaffFile)
         {
             var userId = HttpContext.User.Claims.FirstOrDefault(u => u.Type == ClaimTypes.NameIdentifier)?.Value;
-            await _caffService.ModifyCaffFile(id, modifyCaffFile, userId!);
 
-            return Ok();
+            if (modifyCaffFile.File.Length > 0)
+            {
+                string newFileName = Guid.NewGuid().ToString() + ".caff";
+                string path = $"{pathDir}{newFileName}";
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    modifyCaffFile.File.CopyTo(stream);
+                }
+                await _caffService.ModifyCaffFile(id, modifyCaffFile, userId!, path);
+
+                return Ok();
+            }
+            else
+            {
+                throw new FileNotFoundException("File not found.");
+            }
         }
 
         [Route("new")]
@@ -61,10 +90,26 @@ namespace _3de0.Controllers
         [Authorize(Roles = "admin")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<CaffFilePreviewDto> UploadCaffFile([FromBody] UploadCaffFileDto uploadCaffFile)
+        public async Task<CaffFilePreviewDto> UploadCaffFile([FromForm] UploadCaffFileDto uploadCaffFile)
         {
             var userId = HttpContext.User.Claims.FirstOrDefault(u => u.Type == ClaimTypes.NameIdentifier)?.Value;
-            return await _caffService.UploadCaffFile(uploadCaffFile, userId!);
+
+            if (uploadCaffFile.File.Length > 0)
+            {
+                string newFileName = Guid.NewGuid().ToString() + ".caff";
+                string path = $"{pathDir}{newFileName}";
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    uploadCaffFile.File.CopyTo(stream);
+                }
+                return await _caffService.UploadCaffFile(uploadCaffFile, userId!, path);
+            }
+            else
+            {
+                throw new FileNotFoundException("File not found.");
+            }
+
         }
 
         [Route("delete/{id}")]
@@ -81,9 +126,3 @@ namespace _3de0.Controllers
         }
     }
 }
-
-
-/*----
- * CAFF kommentezés
- * CAFF komment törlés
- * CAFF komment módosítás*/
--- 3de0/3de0_BLL/ICaffService.cs
+++ 3de0/3de0_BLL/ICaffService.cs
@@ -12,10 +12,11 @@ namespace _3de0_BLL
     {
         Task<IEnumerable<CaffFilePreviewDto>> GetCaffFileList();
         Task<CaffFileDto> GetCaffFileDetails(int id);
-        Task ModifyCaffFile(int id, UploadCaffFileDto modifyCaffFile, string userId);
+        Task ModifyCaffFile(int id, UploadCaffFileDto modifyCaffFile, string userId, string path);
         Task RemoveCaffFileById(int id, string userId);
 
-        Task<CaffFilePreviewDto> UploadCaffFile(UploadCaffFileDto uploadCaffFile, string userId);
+        Task<CaffFilePreviewDto> UploadCaffFile(UploadCaffFileDto uploadCaffFile, string userId, string path);
         Task<byte[]> DownloadCaffFile(int id);
+        Task<byte[]> GetPreviewImageByCaffId(int id);
     }
 }
