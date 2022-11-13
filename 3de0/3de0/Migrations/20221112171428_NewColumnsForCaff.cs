3032250654a8d9660f5fcea01125339fada3146c
Surmann Roland <surmannroland@gmail.com>
2022-11-13 18:47:49 +0100

    Database migration

---
+++ 3de0/3de0/Migrations/20221112171428_NewColumnsForCaff.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _3de0.Migrations
+{
+    public partial class NewColumnsForCaff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Files",
+                newName: "Creator");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Caption",
+                table: "Files",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Caption",
+                table: "Files");
+
+            migrationBuilder.RenameColumn(
+                name: "Creator",
+                table: "Files",
+                newName: "Title");
+        }
+    }
+}
