00201d5b1327f2ae8fdd495bda88869488a356dd
Surmann Roland <surmannroland@gmail.com>
2022-11-13 21:17:40 +0100

    New DTO for editing comments

---
+++ 3de0/3de0_BLL/Dtos/ModifyCommentDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3de0_BLL.Dtos
+{
+    public class ModifyCommentDto
+    {
+        public string Content { get; set; }
+        public int Rating { get; set; }
+    }
+}
