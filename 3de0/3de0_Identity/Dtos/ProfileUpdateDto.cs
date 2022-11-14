0f5b882517a2ded6a5e47ccea7a27914ced7f7f2
Varga Kristóf László <varga.kristof.laszlo@gmail.com>
2022-11-14 13:17:26 +0100

    Identy műveletek

---
+++ 3de0/3de0_Identity/Dtos/ProfileUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace _3de0_Identity.Dtos
+{
+    public class ProfileUpdateDto
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
