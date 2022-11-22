643a54cbc8c9de9276c61e57cf355a3f00201163
hegeda <hegeda114@gmail.com>
2022-11-22 21:38:22 +0100

    Provider objects: Create provider objects for downloads.

--- webshop_client/lib/provider_objects.dart
+++ webshop_client/lib/provider_objects.dart
@@ -1,9 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:webshop_client/api/api.dart';
+import 'package:webshop_client/model/downloads_model.dart';
 import 'package:webshop_client/model/user_model.dart';
 import 'package:webshop_client/providers/auth_state_notifier.dart';
+import 'package:webshop_client/providers/downloads_notifier.dart';
 import 'package:webshop_client/providers/shop_notifier.dart';
 import 'package:webshop_client/providers/user_model_notifier.dart';
+import 'package:webshop_client/repository/downloads_repository.dart';
 import 'package:webshop_client/repository/shop_repository.dart';
 import 'model/shop_model.dart';
 import 'repository/auth_repository.dart';
@@ -19,6 +22,10 @@ final shopRepository = Provider<ShopRepository>((ref) {
   return ShopRepository(ref.watch(appRestApi));
 });
 
+final downloadsRepository = Provider<DownloadsRepository>((ref) {
+  return DownloadsRepository(ref.watch(appRestApi));
+});
+
 
 final authStateNotifier = StateNotifierProvider<AuthStateNotifier, AsyncValue<AuthState>>(
     (ref) {
@@ -32,4 +39,8 @@ final userModelNotifier = StateNotifierProvider<UserModelNotifier, UserModel?>((
 
 final shopNotifier = StateNotifierProvider<ShopNotifier, AsyncValue<ShopModel>>((ref) {
   return ShopNotifier(shopRepository: ref.watch(shopRepository));
+});
+
+final downloadsNotifier = StateNotifierProvider<DownloadsNotifier, AsyncValue<DownloadsModel>>((ref) {
+  return DownloadsNotifier(downloadsRepository: ref.watch(downloadsRepository));
 });
